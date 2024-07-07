f1c3a7377bb40629a98f39a2e553485f5387ffd1
Prashant Cholachagudda <pvc@outlook.com>
2024-07-07 17:37:31 +0530

    Update the files

---
+++ MyNativeLib/NativeClass.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MyNativeLib;
+
+public class NativeClass
+{
+    [DllImport("native/libmylibrary.so", EntryPoint = "MyNativeMethod")]
+    public static extern int MyNativeMethod();
+}
---
+++ MyProject/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MyNativeLib;
+
+Console.WriteLine("Calling native method...");
+int result = NativeClass.MyNativeMethod();
+Console.WriteLine($"Result from native method: {result}");
